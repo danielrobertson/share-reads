681f6750c5d2559be27d18c762b1b4804225585c
Daniel Robertson <danielrobertson733@gmail.com>
2025-02-17 20:02:48 -0600

    refactor: deprecate redirect logic from root

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,4 @@
-import { LoaderFunctionArgs } from "@remix-run/cloudflare";
-import { MetaFunction, redirect } from "@remix-run/react";
-import { Client, fql } from "fauna";
-import { commitSession, getSession } from "~/sessions.server";
+import { MetaFunction } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,26 +7,4 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export const loader = async ({ context, request }: LoaderFunctionArgs) => {
-  try {
-    const session = await getSession(request, context.cloudflare.env);
-
-    const client = new Client({
-      secret: context.cloudflare.env.FAUNA_SECRET,
-    });
-
-    const result = await client.query<string>(fql`
-      let newList = BookList.create({ books: [] })
-      newList.id
-    `);
-
-    return redirect(`/lists/edit/${result.data}`, {
-      headers: {
-        "Set-Cookie": await commitSession(session, context.cloudflare.env),
-      },
-    });
-  } catch (error) {
-    console.error("Database connection error:", error);
-    throw new Response("Internal Server Error", { status: 500 });
-  }
-};
+export const loader = async () => {};
