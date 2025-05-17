7e5b8c2136ad1aa58a0e741cdacbb889619d3cca
Daniel Robertson <danielrobertson733@gmail.com>
2025-05-17 17:16:10 -0500

    fix: mobile sidebar overlap issue

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -25,7 +25,7 @@ const Layout = () => {
         <div className="fixed top-16 bottom-0 left-0 z-10">
           <AppSidebar isOpen={isSidebarOpen} />
         </div>
-        <div className={`flex-1 flex flex-col ${isSidebarOpen ? 'md:ml-[232px]' : 'md:ml-16'}`}>
+        <div className={`flex-1 flex flex-col ${isSidebarOpen ? 'md:ml-[232px]' : 'ml-16'}`}>
           <main className="flex-1 overflow-auto p-4 md:p-6 lg:p-8 bg-background">
             <Outlet />
           </main>
